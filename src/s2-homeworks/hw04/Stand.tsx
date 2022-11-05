a83572d86fc31e27b1e107842d2e93389cf7283f
DilanBars2017 <developeryudintsev@gmail.com>
2022-11-05 20:28:13 +0300

    home task 4(refactor8)

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -82,6 +82,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-with-text'}
                         checked={stateForAllCheckboxes}
+                        onChange={() => setChecked(!stateForAllCheckboxes)}
                         onChangeChecked={setChecked}
                     >
                         some text
@@ -92,10 +93,28 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
+                        onChange={() => setChecked(!stateForAllCheckboxes)}
                         onChangeChecked={setChecked}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
                     />
                 </div>
+                {/*<div>*/}
+                {/*    <SuperCheckbox*/}
+                {/*        id={'hw4-super-checkbox-with-text'}*/}
+                {/*        checked={stateForAllCheckboxes}*/}
+                {/*        onChangeChecked={setChecked}*/}
+                {/*    >*/}
+                {/*        some text*/}
+                {/*    </SuperCheckbox>*/}
+                {/*</div>*/}
+                {/*/!*совместим со старым кодом:*!/*/}
+                {/*<div>*/}
+                {/*    <SuperCheckbox*/}
+                {/*        id={'hw4-super-checkbox-like-old'}*/}
+                {/*        checked={stateForAllCheckboxes}*/}
+                {/*        onChangeChecked={setChecked}*/}
+                {/*        onChange={(e) => setChecked(e.currentTarget.checked)}*/}
+                {/*    />*/}
+                {/*</div>*/}
             </div>
         </div>
     )
