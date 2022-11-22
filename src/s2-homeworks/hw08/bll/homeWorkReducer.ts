760207cb66aec0b86b96b5f5cbbe07fefbbf3f59
DilanBars2017 <developeryudintsev@gmail.com>
2022-11-22 22:08:49 +0300

    home task 8(refactor 2)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -13,7 +13,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType) => { // n
         case 'check': {
             // return [state.filter(f=>f.age>18?f:'')] // need to fix
             // console.log(state.filter((f) => f.age>=18),action.payload)
-            return state.filter((f) => f.age>=action.payload) // need to fix
+            return state.filter((a) => a.age >= action.payload) // need to fix
         }
         default:
             return state
