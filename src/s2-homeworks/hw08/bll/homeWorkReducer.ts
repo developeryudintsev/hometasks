edd7d3930f6cf274c5fa974e6fdc0766f89d3366
DilanBars2017 <developeryudintsev@gmail.com>
2022-11-22 22:13:11 +0300

    home task 8(refactor 2)

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,13 +7,15 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType) => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            let arr=action.payload=='up'? state.sort((a, b) => a.name > b.name ? 1 : -1): state.sort((a, b) => a.name < b.name ? 1 : -1)
+            let newState=[...state]
+            let arr=action.payload=='up'? newState.sort((a, b) => a.name > b.name ? 1 : -1): newState.sort((a, b) => a.name < b.name ? 1 : -1)
             return arr // need to fix
         }
         case 'check': {
+            let newState=[...state]
             // return [state.filter(f=>f.age>18?f:'')] // need to fix
             // console.log(state.filter((f) => f.age>=18),action.payload)
-            return state.filter((a) => a.age >= action.payload) // need to fix
+            return newState.filter((a) => a.age >= action.payload) // need to fix
         }
         default:
             return state
