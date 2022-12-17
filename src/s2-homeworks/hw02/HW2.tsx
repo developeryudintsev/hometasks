24e5705f75263c4593c0befa6798af640d006c23
DilanBars2017 <developeryudintsev@gmail.com>
2022-12-17 14:38:11 +0300

    home task 2

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -2,25 +2,10 @@ import React, {useState} from 'react'
 import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 
-/*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
-* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
-* 4 - выполнить пункт 3 для функции deleteAffair
-* 5 - указать нужный тип в useState с affairs
-* 6 - дописать тип и логику функции deleteAffairCallback
-* 7 - в файле Affairs.tsx дописать типизацию пропсов
-* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
-* 9 - в файле Affair.tsx дописать типизацию пропсов
-* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
-* 11 - в файле Affair.tsx отобразить приходящие данные
-* */
-
-// types
-export type AffairPriorityType = 'high'|'middle' |'low' // need to fix any
+export type AffairPriorityType = 'high'|'middle' |'low'
 export type AffairType = {
-    _id: number // need to fix any
-    name: string // need to fix any
+    _id: number
+    name: string
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HW1 from '../../hw01/HW1'
+import HW2 from "../../hw02/HW2";
 
 
 function PreJunior() {
     return (
         <div id={'hw5-page-pre-junior'}>
             {/*pre junior page*/}
-            <HW1 />
-            {/*<HW2 />*/}
+            {/*<HW1 />*/}
+            <HW2 />
             {/*<HW3 />*/}
             {/*<HW4 />*/}
         </div>
